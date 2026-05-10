7f337f3fbc1a7f6f4bb9171106dcb9304f691f26
ATATC <futerry@outlook.com>
2026-05-09 20:54:06 -0400

    Update SLURM script: reduce job runtime to 1 hour for preprocessing task

--- jobs/preprocess_flare.sh
+++ jobs/preprocess_flare.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_1g.10gb:1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
-#SBATCH --time=02:00:00
+#SBATCH --time=01:00:00
 #SBATCH --output=logs/%x_%j.out
 #SBATCH --error=logs/%x_%j.err
 
